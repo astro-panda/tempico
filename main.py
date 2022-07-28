b2af0b67eb287cfa3e281a0ad19217336c15569b
Turner Bass <turner@astropanda.studio>
2022-07-28 14:08:40 -0500

    Added main.py and temp sensor driver

---
+++ main.py
@@ -0,0 +1,42 @@
+# DHT22 Sensor pins
+# * DHT22 Pin 1 to 3V3
+# * DHT22 Pin 2 to GP2
+# * DHT22 Pin 3 to NC
+# * DHT22 Pin 4 to GND
+
+from machine import Pin, UART
+from DHT22 import DHT22
+import time
+
+# Initialize DHT22
+dht22 = DHT22(Pin(2,Pin.IN,Pin.PULL_UP))
+
+#Initialize the onboard LED as output
+led = machine.Pin(25,machine.Pin.OUT)
+
+# init with given baudrate
+uart = UART(1, 9600)
+# init with given parameters
+uart.init(9600, bits=8, parity=None, stop=1)
+
+while True:
+    led.value(1)
+    T, H = dht22.read()
+    
+    humid = H
+    #Write Humidity value. Convert the humidity into two decimal places.
+    print('Humidity: ' + "{:0.2f}".format(humid) + "%")
+    
+    temp = (T * 9/5) + 32
+    #Write Temperature value and convert the temperature.
+    print('Temp: ' + str(temp) + "F")
+    print('')
+    
+    # Try to write to COM
+    uart.write(str(temp) + ',' + str(humid) + '\n')
+
+    time.sleep_ms(500)
+    led.value(0)
+
+    # Wait for Five seconds. Then proceed to collect next sensor reading.
+    time.sleep_ms(5000)
